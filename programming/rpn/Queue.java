e5b1676ebdd3fa1a7438b4e8237c140a9453c39e
Sophia Quick <Skipper@Sophias-MacBook-Pro-2.local>
2017-12-15 09:28:45 -0700

    Working Queue

--- programming/rpn/Queue.java
+++ programming/rpn/Queue.java
@@ -5,7 +5,10 @@ public class Queue<T> implements IQueue<T>{
   int size = 0;
 
   public T dequeue() throws UnderFlowException{
-    if (back == null){ throw new UnderFlowException();}
+    if (front == null){
+      System.out.println("Empty Queue");
+      return null;
+    }
     T v = front.getValue();
     front = front.getPointer();
     size--;
@@ -13,12 +16,20 @@ public class Queue<T> implements IQueue<T>{
   }
 
   public void enqueue(T v) throws OverFlowException{
-    Cell<T> n = new Cell<T>(v, front);
+    Cell<T> n = new Cell<T>(v, back);
     if (size == 0){
       front = n;
     }
     back = n;
     size++;
+  }
+
+  public static void main(String[] args) throws UnderFlowException, OverFlowException{
+    Queue<Integer> q = new Queue();
+    q.enqueue(5);
+    q.enqueue(15);
+    Integer dq = q.dequeue();
+    System.out.println(dq);
   }
 
 }
