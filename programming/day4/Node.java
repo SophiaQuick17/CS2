0c952c6ade24cc94e0ad0d6a3328e2f5792cff03
SophiaQuick17 <33337497+SophiaQuick17@users.noreply.github.com>
2017-12-19 08:02:34 -0700

    code

---
+++ programming/day4/Node.java
@@ -0,0 +1,37 @@
+public class Node<T> implements IDLink<T>{
+
+  IDLink<T> next;
+  IDLink<T> previous;
+  T value;
+
+  public Node(T v, IDLink<T> n, IDLink p){
+    next = n;
+    previous = p;
+    value = v;
+  }
+
+  public T getValue(){
+    return value;
+  }
+
+  public void setValue(T v){
+    value = v;
+  }
+
+  public IDLink<T> getNext(){
+    return next;
+  }
+
+  public IDLink<T> getPrev(){
+    return previous;
+  }
+
+  public void setNext(IDLink<T> c){
+    next = c;
+  }
+
+  public void setPrev(IDLink<T> c){
+    previous = c;
+  }
+
+}
